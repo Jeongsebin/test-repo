16f5a3803b0415e15b3f04da3b3aa5e21f5659f6
Jeongsebin <101381141+Jeongsebin@users.noreply.github.com>
2022-05-17 22:38:53 +0900

    Add files via upload

---
+++ moneyrate.py
@@ -0,0 +1,8 @@
+money = int(input('환전하고자 하는 금액을 입력하세요 : '))
+usd = money / 1141.50
+aud = money / 905.15
+nzd = money / 836.15
+
+print('미국 :', round(usd,2), 'USD')
+print('호주 :', round(aud,2), 'AUD')
+print('뉴질랜드 :', round(nzd,2), 'NZD')
