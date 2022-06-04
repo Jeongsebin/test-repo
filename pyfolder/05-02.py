6e87cf26d424bfc8946837d467a51124eb55db7b
Jeongsebin <101381141+Jeongsebin@users.noreply.github.com>
2022-06-04 21:21:55 +0900

    Add files via upload

---
+++ pyfolder/05-02.py
@@ -0,0 +1,16 @@
+flag = input('마음에 드는 옷을 찾았나요?(예/아니오): ')
+
+if flag == '예':
+    print(':) 축하합니다!!')
+
+    price = input('가격이 얼마인가요?: ')
+
+    if int(price) <= 100000:
+        print('구매합니다.')
+    else:
+        print('포기합니다.')
+
+elif flag == '아니오':
+    print(':( 아쉽군요.')
+else:
+    print("'예'또는 '아니오'만 입력하시오")
