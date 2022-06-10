6bbfc59761c5badda362c546a8cec9593c5b07bf
Jeongsebin <101381141+Jeongsebin@users.noreply.github.com>
2022-06-10 15:43:02 +0900

    Add files via upload

---
+++ pyfolder/fnld.py
@@ -0,0 +1,33 @@
+while True:
+	print("1. 더하기")
+	print("2. 빼기")
+	print("3. 곱하기")
+	print("4. 나누기")
+	print("5. 프로그램 종료")
+	menu = int(input("원하는 기능의 번호를 입력하세요: "))
+	
+	if menu == 1:
+		num1 = int(input('첫 번째 정수를 입력하세요: '))
+		num2 = int(input('두 번째 정수를 입력하세요: '))
+		print('{0}와 {1}를 더하기 연산한 결과는 {2}입니다.'.format(num1, num2, num1+num2))
+	elif menu == 2:
+		num1 = int(input('첫 번째 정수를 입력하세요: '))
+		num2 = int(input('두 번째 정수를 입력하세요: '))
+		print('{0}와 {1}를 빼기 연산한 결과는 {2}입니다.'.format(num1, num2, num1-num2))
+	elif menu == 3:
+		num1 = int(input('첫 번째 정수를 입력하세요: '))
+		num2 = int(input('두 번째 정수를 입력하세요: '))
+		print('{0}와 {1}를 곱하기 연산한 결과는 {2}입니다.'.format(num1, num2, num1*num2))
+	elif menu == 4:
+		num1 = int(input('첫 번째 정수를 입력하세요: '))
+		num2 = int(input('두 번째 정수를 입력하세요: '))
+		if num2 == 0:
+			print('연산이 불가합니다.')
+		else:
+			print('%d를 %d로 나누기 연산한 결과는 %f입니다.'%(num1, num2, num1/num2))
+	elif menu == 5:
+		print('프로그램을 종료합니다.')
+		break
+	else:
+		print('잘못된 번호입니다.')
+	
